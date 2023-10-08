7c5f0463d8e2bd176710e7a45c2e3e4842333b6a
Petrov Dmitriy <dmitr.petrov.spb@gmail.com>
2023-10-08 18:07:34 +0300

    deleted unused import

--- src/main/java/io/ylab/dao/transaction/TransactionRepository.java
+++ src/main/java/io/ylab/dao/transaction/TransactionRepository.java
@@ -2,8 +2,6 @@ package io.ylab.dao.transaction;
 
 import io.ylab.model.Transaction;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public interface TransactionRepository {
     void addTransaction(Transaction transaction);
