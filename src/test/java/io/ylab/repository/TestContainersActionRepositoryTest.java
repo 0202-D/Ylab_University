eed17634afe7f2eca7b430c71cc8d1fb00843918
Petrov Dmitriy <dmitr.petrov.spb@gmail.com>
2023-10-15 12:02:18 +0300

    add action repository test

--- src/test/java/io/ylab/repository/TestContainersActionRepositoryTest.java
+++ src/test/java/io/ylab/repository/TestContainersActionRepositoryTest.java
@@ -1,4 +1,85 @@
 package io.ylab.repository;
 
-public class TestContainersActionRepositoryTest {
+import io.ylab.Utils;
+import io.ylab.dao.action.JdbcActionRepository;
+import io.ylab.model.Action;
+import io.ylab.model.User;
+import io.ylab.utils.DataBaseConnector;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TestContainersActionRepositoryTest {
+    private PostgreSQLContainer<?> container;
+    private Connection connection;
+    private JdbcActionRepository actionRepository;
+    private final static String CREATE_SCHEMA_IF_NOT_EXISTS_DOMAIN = "CREATE SCHEMA IF NOT EXISTS domain";
+    private final static String CREATE_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS domain.user" +
+            " (user_id SERIAL PRIMARY KEY, user_name VARCHAR(255),password varchar(255),balance decimal)";
+    private final static String CREATE_TABLE_QUERY_TWO = "CREATE TABLE IF NOT EXISTS domain.action" +
+            " (action_id SERIAL PRIMARY KEY, user_id bigint,activity VARCHAR(255))";
+    private final static String INSERT = "INSERT INTO domain.user (user_name,password,balance) VALUES(?,?,?)";
+
+    @BeforeEach
+    public void setup() throws SQLException {
+        container = new PostgreSQLContainer<>("postgres:latest");
+        container.start();
+        String jdbcUrl = container.getJdbcUrl();
+        String username = container.getUsername();
+        String password = container.getPassword();
+        connection = DataBaseConnector.updateConnectionProperties(jdbcUrl, username, password);
+        try (PreparedStatement statement = connection.prepareStatement(CREATE_SCHEMA_IF_NOT_EXISTS_DOMAIN)) {
+            statement.executeUpdate();
+        }
+        try (PreparedStatement statement = connection.prepareStatement(CREATE_TABLE_QUERY)) {
+            statement.executeUpdate();
+        }
+        try (PreparedStatement statement = connection.prepareStatement(INSERT)) {
+            statement.setString(1, "username");
+            statement.setString(2, "password");
+            statement.setBigDecimal(3, BigDecimal.valueOf(100));
+            statement.executeUpdate();
+            statement.executeUpdate();
+        }
+        try (PreparedStatement statement = connection.prepareStatement(CREATE_TABLE_QUERY_TWO)) {
+            statement.executeUpdate();
+        }
+        actionRepository = new JdbcActionRepository();
+    }
+
+    @AfterEach
+    public void cleanup() throws SQLException {
+        connection.close();
+        container.stop();
+    }
+    @Test
+    void testAddAction() {
+        // Создание и добавление действия в базу данных
+        User user = Utils.getUser();
+        Action action = Utils.getEnterAction(user);
+        actionRepository.addAction(action);
+        List<Action> actions = actionRepository.getAllByUserName(user.getUserName());
+        assertTrue(actions.contains(action));
+    }
+    @Test
+    void testGetAllByUserName() {
+        User user = Utils.getUser();
+        Action action1 = Utils.getEnterAction(user);
+        Action action2 = Utils.getCreditAction(user);
+        actionRepository.addAction(action1);
+        actionRepository.addAction(action2);
+        List<Action> actions = actionRepository.getAllByUserName(user.getUserName());
+        assertEquals(2,actions.size());
+    }
 }
+
