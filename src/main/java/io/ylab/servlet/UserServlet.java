f32ec73f9141868136cb1b4d9a28bf65717178a3
Petrov Dmitriy <dmitr.petrov.spb@gmail.com>
2023-10-26 19:09:08 +0300

    fix authentication

--- src/main/java/io/ylab/servlet/UserServlet.java
+++ src/main/java/io/ylab/servlet/UserServlet.java
@@ -36,6 +36,8 @@ import java.util.Set;
 public class UserServlet extends HttpServlet {
     public static final String APPLICATION_JSON = "application/json";
     private static final String NOT_FOUND = "Такого пользователя не существует";
+
+    private static final String ACCESS_DENIED = "Доступ запрещен!";
     private JwtProvider jwtProvider = new JwtProvider();
     private final ObjectMapper objectMapper;
     private final ActionRepository actionRepository;
@@ -69,126 +71,132 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        jwtProvider.validateJwtToken(req);
-        String requestURI = req.getRequestURI();
-        if (requestURI.contains("user/balance")) {
-            long userId = getUserId(req);
-            UserBalanceRs userBalanceRs = userController.balance(userId);
-            if (userBalanceRs == null) {
-                ExceptionJson exceptionJson = ExceptionJson.builder()
-                        .message(NOT_FOUND)
-                        .httpResponse(HttpServletResponse.SC_NOT_FOUND)
-                        .build();
-                resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-                resp.setContentType(APPLICATION_JSON);
-                resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(exceptionJson));
-            } else {
-                resp.setStatus(HttpServletResponse.SC_OK);
-                resp.setContentType(APPLICATION_JSON);
-                resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(userBalanceRs));
-            }
-        } else if (requestURI.contains("/user/history/")) {
-            long userId = getUserId(req);
-            List<TransactionHistoryDtoRs> history = userController.history(userId);
-            if (history == null) {
-                ExceptionJson exceptionJson = ExceptionJson.builder()
-                        .message(NOT_FOUND)
-                        .httpResponse(HttpServletResponse.SC_NOT_FOUND)
-                        .build();
-                resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-                resp.setContentType(APPLICATION_JSON);
-                resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(exceptionJson));
-            } else {
-                resp.setStatus(HttpServletResponse.SC_OK);
-                resp.setContentType(APPLICATION_JSON);
-                resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(history));
-            }
-        } else if (requestURI.contains("/user/activity")) {
-            long userId = getUserId(req);
-            List<ActivityRs> activity = userController.activity(userId);
-            if (activity == null) {
-                ExceptionJson exceptionJson = ExceptionJson.builder()
-                        .message(NOT_FOUND)
-                        .httpResponse(HttpServletResponse.SC_NOT_FOUND)
-                        .build();
-                resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-                resp.setContentType(APPLICATION_JSON);
-                resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(exceptionJson));
-            } else {
-                resp.setStatus(HttpServletResponse.SC_OK);
-                resp.setContentType(APPLICATION_JSON);
-                resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(activity));
+        if (!jwtProvider.validateJwtToken(req)) {
+            returnForbiddenResponse(resp);
+        } else {
+            String requestURI = req.getRequestURI();
+            if (requestURI.contains("user/balance")) {
+                long userId = getUserId(req);
+                UserBalanceRs userBalanceRs = userController.balance(userId);
+                if (userBalanceRs == null) {
+                    ExceptionJson exceptionJson = ExceptionJson.builder()
+                            .message(NOT_FOUND)
+                            .httpResponse(HttpServletResponse.SC_NOT_FOUND)
+                            .build();
+                    resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                    resp.setContentType(APPLICATION_JSON);
+                    resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(exceptionJson));
+                } else {
+                    resp.setStatus(HttpServletResponse.SC_OK);
+                    resp.setContentType(APPLICATION_JSON);
+                    resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(userBalanceRs));
+                }
+            } else if (requestURI.contains("/user/history/")) {
+                long userId = getUserId(req);
+                List<TransactionHistoryDtoRs> history = userController.history(userId);
+                if (history == null) {
+                    ExceptionJson exceptionJson = ExceptionJson.builder()
+                            .message(NOT_FOUND)
+                            .httpResponse(HttpServletResponse.SC_NOT_FOUND)
+                            .build();
+                    resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                    resp.setContentType(APPLICATION_JSON);
+                    resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(exceptionJson));
+                } else {
+                    resp.setStatus(HttpServletResponse.SC_OK);
+                    resp.setContentType(APPLICATION_JSON);
+                    resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(history));
+                }
+            } else if (requestURI.contains("/user/activity")) {
+                long userId = getUserId(req);
+                List<ActivityRs> activity = userController.activity(userId);
+                if (activity == null) {
+                    ExceptionJson exceptionJson = ExceptionJson.builder()
+                            .message(NOT_FOUND)
+                            .httpResponse(HttpServletResponse.SC_NOT_FOUND)
+                            .build();
+                    resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                    resp.setContentType(APPLICATION_JSON);
+                    resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(exceptionJson));
+                } else {
+                    resp.setStatus(HttpServletResponse.SC_OK);
+                    resp.setContentType(APPLICATION_JSON);
+                    resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(activity));
+                }
             }
         }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        jwtProvider.validateJwtToken(req);
-        String requestURI = req.getRequestURI();
-        if (requestURI.contains("/user/credit")) {
-            resp.setContentType(APPLICATION_JSON);
-            final var gson = new Gson();
-            var body = req.getReader();
-            final var creditRq = gson.fromJson(body, CreditAndDebitRq.class);
-            Set<ConstraintViolation<CreditAndDebitRq>> violations = validator.validate(creditRq);
-            if (!violations.isEmpty()) {
-                List<String> errors = new ArrayList<>();
-                for (ConstraintViolation<CreditAndDebitRq> violation : violations) {
-                    resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                    ExceptionJson exceptionJson = ExceptionJson.builder()
-                            .message(violation.getMessage())
-                            .httpResponse(HttpServletResponse.SC_BAD_REQUEST)
-                            .build();
-                    resp.setContentType(APPLICATION_JSON);
-                    resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(exceptionJson));
-                }
-            } else {
-                boolean isOk = userController.credit(creditRq);
-                if (!isOk) {
-                    CreditAndDebitRs creditAndDebitRs = CreditAndDebitRs.builder()
-                            .httpResponse(HttpServletResponse.SC_BAD_REQUEST).build();
-                    resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                    resp.setContentType(APPLICATION_JSON);
-                    resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(creditAndDebitRs));
+        if (!jwtProvider.validateJwtToken(req)) {
+            returnForbiddenResponse(resp);
+        } else {
+            String requestURI = req.getRequestURI();
+            if (requestURI.contains("/user/credit")) {
+                resp.setContentType(APPLICATION_JSON);
+                final var gson = new Gson();
+                var body = req.getReader();
+                final var creditRq = gson.fromJson(body, CreditAndDebitRq.class);
+                Set<ConstraintViolation<CreditAndDebitRq>> violations = validator.validate(creditRq);
+                if (!violations.isEmpty()) {
+                    List<String> errors = new ArrayList<>();
+                    for (ConstraintViolation<CreditAndDebitRq> violation : violations) {
+                        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                        ExceptionJson exceptionJson = ExceptionJson.builder()
+                                .message(violation.getMessage())
+                                .httpResponse(HttpServletResponse.SC_BAD_REQUEST)
+                                .build();
+                        resp.setContentType(APPLICATION_JSON);
+                        resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(exceptionJson));
+                    }
                 } else {
-                    CreditAndDebitRs creditAndDebitRs = CreditAndDebitRs.builder()
-                            .httpResponse(HttpServletResponse.SC_OK).build();
-                    resp.setContentType(APPLICATION_JSON);
-                    resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(creditAndDebitRs));
+                    boolean isOk = userController.credit(creditRq);
+                    if (!isOk) {
+                        CreditAndDebitRs creditAndDebitRs = CreditAndDebitRs.builder()
+                                .httpResponse(HttpServletResponse.SC_BAD_REQUEST).build();
+                        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                        resp.setContentType(APPLICATION_JSON);
+                        resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(creditAndDebitRs));
+                    } else {
+                        CreditAndDebitRs creditAndDebitRs = CreditAndDebitRs.builder()
+                                .httpResponse(HttpServletResponse.SC_OK).build();
+                        resp.setContentType(APPLICATION_JSON);
+                        resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(creditAndDebitRs));
+                    }
                 }
             }
-        }
-        if (requestURI.contains("/user/debit")) {
-            resp.setContentType(APPLICATION_JSON);
-            final var gson = new Gson();
-            var body = req.getReader();
-            final var debitRq = gson.fromJson(body, CreditAndDebitRq.class);
-            Set<ConstraintViolation<CreditAndDebitRq>> violations = validator.validate(debitRq);
-            if (!violations.isEmpty()) {
-                List<String> errors = new ArrayList<>();
-                for (ConstraintViolation<CreditAndDebitRq> violation : violations) {
-                    resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                    ExceptionJson exceptionJson = ExceptionJson.builder()
-                            .message(violation.getMessage())
-                            .httpResponse(HttpServletResponse.SC_BAD_REQUEST)
-                            .build();
-                    resp.setContentType(APPLICATION_JSON);
-                    resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(exceptionJson));
-                }
-            } else {
-                boolean isOk = userController.debit(debitRq);
-                if (!isOk) {
-                    CreditAndDebitRs creditAndDebitRs = CreditAndDebitRs.builder()
-                            .httpResponse(HttpServletResponse.SC_BAD_REQUEST).build();
-                    resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                    resp.setContentType(APPLICATION_JSON);
-                    resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(creditAndDebitRs));
+            if (requestURI.contains("/user/debit")) {
+                resp.setContentType(APPLICATION_JSON);
+                final var gson = new Gson();
+                var body = req.getReader();
+                final var debitRq = gson.fromJson(body, CreditAndDebitRq.class);
+                Set<ConstraintViolation<CreditAndDebitRq>> violations = validator.validate(debitRq);
+                if (!violations.isEmpty()) {
+                    List<String> errors = new ArrayList<>();
+                    for (ConstraintViolation<CreditAndDebitRq> violation : violations) {
+                        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                        ExceptionJson exceptionJson = ExceptionJson.builder()
+                                .message(violation.getMessage())
+                                .httpResponse(HttpServletResponse.SC_BAD_REQUEST)
+                                .build();
+                        resp.setContentType(APPLICATION_JSON);
+                        resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(exceptionJson));
+                    }
                 } else {
-                    CreditAndDebitRs creditAndDebitRs = CreditAndDebitRs.builder()
-                            .httpResponse(HttpServletResponse.SC_OK).build();
-                    resp.setContentType(APPLICATION_JSON);
-                    resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(creditAndDebitRs));
+                    boolean isOk = userController.debit(debitRq);
+                    if (!isOk) {
+                        CreditAndDebitRs creditAndDebitRs = CreditAndDebitRs.builder()
+                                .httpResponse(HttpServletResponse.SC_BAD_REQUEST).build();
+                        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                        resp.setContentType(APPLICATION_JSON);
+                        resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(creditAndDebitRs));
+                    } else {
+                        CreditAndDebitRs creditAndDebitRs = CreditAndDebitRs.builder()
+                                .httpResponse(HttpServletResponse.SC_OK).build();
+                        resp.setContentType(APPLICATION_JSON);
+                        resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(creditAndDebitRs));
+                    }
                 }
             }
         }
@@ -202,4 +210,13 @@ public class UserServlet extends HttpServlet {
         return userId;
     }
 
+    private void returnForbiddenResponse(HttpServletResponse resp) throws IOException {
+        ExceptionJson exceptionJson = ExceptionJson.builder()
+                .message(ACCESS_DENIED)
+                .httpResponse(HttpServletResponse.SC_FORBIDDEN)
+                .build();
+        resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        resp.setContentType(APPLICATION_JSON);
+        resp.getOutputStream().write(this.objectMapper.writeValueAsBytes(exceptionJson));
+    }
 }
