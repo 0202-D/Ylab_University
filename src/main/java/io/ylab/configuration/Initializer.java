eaadadcbb8c4bf6716b7f3820d600efe1b3b4d28
Petrov Dmitriy <dmitr.petrov.spb@gmail.com>
2023-10-29 13:45:26 +0300

    add config

---
+++ src/main/java/io/ylab/configuration/Initializer.java
@@ -0,0 +1,4 @@
+package io.ylab.configuration;
+
+public class Initializer {
+}
---
+++ src/main/java/io/ylab/configuration/WalletConfig.java
@@ -0,0 +1,70 @@
+package io.ylab.configuration;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = "io.ylab")
+@EnableWebMvc
+@EnableTransactionManagement
+@PropertySource("classpath:application.yml")
+public class WalletConfig {
+    @Bean
+    DataSource dataSource(){
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("org.postgresql.Driver");
+            dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/wallet");
+            dataSource.setUser("postgres");
+            dataSource.setPassword("postgres");
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+        return dataSource;
+    }
+    @Bean
+    LocalSessionFactoryBean sessionFactory(){
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+        sessionFactoryBean.setPackagesToScan("io.ylab");
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect","org.hibernate.dialect.PostgreSQL95Dialect");
+        hibernateProperties.setProperty("hibernate.show_sql","true");
+        sessionFactoryBean.setHibernateProperties(hibernateProperties);
+        return sessionFactoryBean;
+    }
+    @Bean
+    HibernateTransactionManager transactionManager(){
+        HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
+        hibernateTransactionManager.setSessionFactory(sessionFactory().getObject());
+        return hibernateTransactionManager;
+    }
+    @Bean
+    public YamlPropertiesFactoryBean yamlProperties() {
+        YamlPropertiesFactoryBean factoryBean = new YamlPropertiesFactoryBean();
+        factoryBean.setResources(new ClassPathResource("application.yml"));
+        return factoryBean;
+    }
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:db/changelog/changelog.xml");
+        return liquibase;
+    }
+}
+
