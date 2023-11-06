ce7528d403a787ce18289f319994017f591117c7
Petrov Dmitriy <dmitr.petrov.spb@gmail.com>
2023-11-06 16:16:30 +0300

    some improvements

--- src/main/java/io/ylab/Ylab_UniversityApplication.java
+++ src/main/java/io/ylab/Ylab_UniversityApplication.java
@@ -1,11 +1,9 @@
 package io.ylab;
 
-import io.ylab.auditaspectstarter.aop.annotation.EnableAuditStarter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAuditStarter
 public class Ylab_UniversityApplication {
     public static void main(String[] args) {
         SpringApplication.run(Ylab_UniversityApplication.class, args);
